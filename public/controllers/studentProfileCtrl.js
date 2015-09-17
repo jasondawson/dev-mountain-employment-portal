bc0f380809881011da522efe362ce0ba73406f90
adriana <rubi12wera@gmail.com>
2015-09-16 20:16:43 -0600

    student profile form

--- public/controllers/studentProfileCtrl.js
+++ public/controllers/studentProfileCtrl.js
@@ -11,38 +11,7 @@ app.controller("studentProfileCtrl", function($scope, studentProfileSvc, $filter
 	};
 
 	$scope.getStudentProf();
-/*
-$scope.url='';
-$scope.checkforUrl=function(studentData){
-	if (studentData.studentPortf.github !== null) {
-		return $scope.url = studentData.studentPortf.github;
-	} else{
-		$scope.url = "http://github.com"
-	};
-}
-$scope.checkforUrl();*/
- //var studentpicture=$scope.s3url; see how to save puc url from amazon to our student profile 
 
-	
-  $scope.user = {
-    name: 'Name',
-    midle:"M",
-    lastname:'Last-Name',
-    email:'your@email.com',
-    pictureurl:'Upload you picture here',
-    github: '',
-    personalWebsite: 'Personal Website',
-    linkdIn: 'Share your LinkdIn',
-    Bio: 'Here is where you tell us about you',
- // link or upload to amazon s3 that saves link to resume.
-   currentLoc: {
-    city: 'City',
-    state: 'State',
-  	},
-  	status: 0,
-  	cohortLoc:0,
-
-  }; 
   $scope.statuses =[
   	{value:0, text:'Status'},
   	{value:1, text:'Student'},
@@ -79,3 +48,35 @@ $scope.checkforUrl();*/
   
 
 });
+/* Fake DATA
+$scope.url='';
+$scope.checkforUrl=function(studentData){
+	if (studentData.studentPortf.github !== null) {
+		return $scope.url = studentData.studentPortf.github;
+	} else{
+		$scope.url = "http://github.com"
+	};
+}
+$scope.checkforUrl();*/
+ //var studentpicture=$scope.s3url; see how to save puc url from amazon to our student profile 
+
+	
+/*  $scope.user = {
+    name: 'Name',
+    midle:"M",
+    lastname:'Last-Name',
+    email:'your@email.com',
+    pictureurl:'Upload you picture here',
+    github: '',
+    personalWebsite: 'Personal Website',
+    linkdIn: 'Share your LinkdIn',
+    Bio: 'Here is where you tell us about you: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+ // link or upload to amazon s3 that saves link to resume.
+   currentLoc: {
+    city: 'City',
+    state: 'State',
+  	},
+  	status: 0,
+  	cohortLoc:0,
+
+  }; */
