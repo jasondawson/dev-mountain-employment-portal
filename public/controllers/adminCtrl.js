f8fc96057812a85618bfee6af900120e3c2b99d3
caulkerman <ih8blackwidowspiders@yahoo.com>
2015-09-22 14:24:23 -0600

    some changes to admin

--- public/controllers/adminCtrl.js
+++ public/controllers/adminCtrl.js
@@ -1,10 +1,8 @@
-app.controller("adminCtrl", function($scope, adminSvc, $timeout, $mdUtil, $mdSidenav, $log) {
+app.controller("adminCtrl", function($scope, adminSvc, cohortLocServ, $timeout, $mdUtil, $mdSidenav, $log) {
 
 
+//////SLIDER NAV BAR FUNCTION////////
 
-// $scope.checkboxModel = {
-//        value1 : false,
-//      };
 $scope.toggleLeft = buildToggler('left');
 function buildToggler(navID) {
       var debounceFn =  $mdUtil.debounce(function(){
@@ -23,7 +21,9 @@ function buildToggler(navID) {
         });
     };
 
-$scope.adminReadStudents = function() {
+/////CRUD FUNCTIONS//////
+
+$scope.adminReadStudents = function() { //this functions gets the student profiles
 	adminSvc.adminReadStudents().then(function(response) {
 		console.log("response from controller", response.data);
 		$scope.students = response.data;
@@ -32,7 +32,7 @@ $scope.adminReadStudents = function() {
 $scope.adminReadStudents();
 
 
-$scope.showProfile = function(student) {
+$scope.showProfile = function(student) {  ////this function changes boolean in database for the checkboxes
 	console.log(student)
 	adminSvc.adminUpdateShowStudent(student).then(function(response) {
 		if (response.status === 200) {
@@ -41,7 +41,7 @@ $scope.showProfile = function(student) {
 	});
 };
 
-$scope.adminUpdatePercent = function(percent, index, _id) {
+$scope.adminUpdatePercent = function(percent, index, _id) {  ////this function changes/updates the percent value
 	$scope.students[index].percentCompleted = percent;
 	if (percent < 0 || percent > 100) {
 		alert("Enter a number between 0 and 100. Your incorrect input did not save to the database");
@@ -55,6 +55,28 @@ $scope.adminUpdatePercent = function(percent, index, _id) {
 		});
 	}
 };
+
+var getCohortLocations = function() { ///tis function gets cohort locations from DB
+	cohortLocServ.getCohortLoc().then(function(response) {
+		if (response.status === 200) {
+			$scope.cohortLocations = response.data;
+			// console.log("cohort locations response on controller from the service", response.data);
+		}
+	})
+}
+getCohortLocations();
+
+$scope.addNewLocation = function(newLoc) {
+	console.log("this is the newLoc before it goes to service", newLoc);
+	cohortLocServ.addCohortLoc(newLoc).then(function(response) {
+		console.log("new location response", response)
+		if (response === 200) {
+			$scope.newLoc = "";
+			console.log("new location added");
+		}
+	})
+	getCohortLocations();
+}
 
 // $scope.delete = function(var, _id) {
 // 	adminSvc.delete(var, _id).then(function(response) {
