47a0da2d7c8fc5227dc7f81a7edf4a0d378492a4
Janice <findjanice@gmail.com>
2015-09-22 15:29:57 -0600

    directive working

--- public/directives/porfoliohover.js
+++ public/directives/porfoliohover.js
@@ -4,9 +4,25 @@ app.directive('portfolioView', function() {
     scope: {
       student: "="
     },
-    template: '<div class="outer-box">Name: {{student.name.first}} {{student.name.last}}<br>Bio: {{student.Bio}} <br>Skills: {{student.skills}}</div>'
-      // link: function(scope, elems, attrs) {
-      //   console.log(scope.datasource);
-      // }
+    template: '<div class="outer-box"><img src="{{student.picture}}" alt="{{student.name.first}} {{student.name.last}}" style="width: 200px; height: 200px">Name: {{student.name.first}} {{student.name.last}}<br>Bio: {{student.Bio}} <br>Skills: {{student.skills}}<br></div>',
+    link: function(scope, elem, attrs) {
+      console.log('this is scope link', scope);
+      var test = scope.student;
+      var newHtml = '<div class="outer-box"> Personal Website: ' + test.personalWebsite +
+        '</div>';
+      console.log('this is test', test);
+      elem.on('click', function() {
+        elem.html(
+          newHtml
+        )
+      })
+    }
+
+    // link: function(scope, elems, attrs) {
+    //   elem.on('mouseenter/exit', function() {
+    //     elem.html('new info goes here');
+    //   });
+    // }
+    //$location
   }
 });
