5d3ce408ef4ed42b406e3f98912375c232de0354
caulkerman <ih8blackwidowspiders@yahoo.com>
2015-09-22 11:12:38 -0600

    mostly admin page changes, have slider working, deleted one route.

--- public/controllers/adminCtrl.js
+++ public/controllers/adminCtrl.js
@@ -1,26 +1,27 @@
-app.controller("adminCtrl", function($scope, adminSvc) {
-
-// app.controller('adminCtrl', ['$scope', function (scope, adminSvc) {
-    
-
-    // $scope.rowCollection = [
-    //     {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
-    //     {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
-    //     {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
-    // ];
-// });
+app.controller("adminCtrl", function($scope, adminSvc, $timeout, $mdUtil, $mdSidenav, $log) {
 
 
-// $scope.create = function(var) {
-// 	adminSvc.create(var).then(function(response) {
 
-// 	});
-// };
-
-$scope.checkboxModel = {
-       value1 : false,
-     };
-
+// $scope.checkboxModel = {
+//        value1 : false,
+//      };
+$scope.toggleLeft = buildToggler('left');
+function buildToggler(navID) {
+      var debounceFn =  $mdUtil.debounce(function(){
+            $mdSidenav(navID)
+              .toggle()
+              .then(function () {
+                $log.debug("toggle " + navID + " is done");
+              });
+          },200);
+      return debounceFn;
+    }
+     $scope.close = function () {
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+    };
 
 $scope.adminReadStudents = function() {
 	adminSvc.adminReadStudents().then(function(response) {
