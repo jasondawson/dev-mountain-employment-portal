46caafc488fe4a88a4846ba966705e1c4313e360
Janice <findjanice@gmail.com>
2015-09-23 20:06:45 -0600

    hover working, added filter on porfolio page

--- public/directives/porfoliohover.js
+++ public/directives/porfoliohover.js
@@ -5,48 +5,14 @@ app.directive('portfolioView', function() {
       student: "="
     },
     templateUrl: '/html-templates/hoverPortfolio.html',
-    link: function(scope, elem, attrs) {
-      //gets the first project and shows it
-      var project = scope.student.projects;
-      var firstProject = project[0];
-      var fp_name = firstProject.name;
-      var fp_type = firstProject.projectType;
-      var fp_description = firstProject.description;
-      //gets the second project and shows it
-      var secondProject = project[1];
-      var sp_name = secondProject.name;
-      var sp_type = secondProject.projectType;
-      var sp_description = secondProject.description;
-      //the template that shows the second project
-      var newHtml =
-        '<div class="projects outer-box"><div class="firstproject"> Project Name: ' +
-        fp_name + '<br>Type: ' + fp_type + '<br>Description: ' +
-        fp_description +
-        '</div><br><div class="secondproject"> Project Name: ' +
-        sp_name + '<br>Type: ' + sp_type + '<br>Description: ' +
-        sp_description +
-        '</div> </div>';
-
-      var oldHtml = "";
-
-      elem.on('mouseenter', function() {
-        elem.html(
-          newHtml
-        )
-      });
-
-      elem.on('mouseleave', function() {
-        elem.html(
-          oldHtml
-        )
-      });
+    controller: function($scope) {
+      $scope.hidden = false;
+      $scope.showHidden = function(e) {
+        $scope.hidden = true;
+      }
+      $scope.hide = function(e) {
+        $scope.hidden = false;
+      }
     }
-
-    // link: function(scope, elems, attrs) {
-    //   elem.on('mouseenter/exit', function() {
-    //     elem.html('new info goes here');
-    //   });
-    // }
-    //$location
   }
 });
